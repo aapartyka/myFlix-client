97e9bb0257b48342f8b00cb230a1dd111c1c2296
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-17 18:07:31 +0100

    update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
   import { MovieCard} from '../movie-card/movie-card';
   import {MovieView} from '../movie-view/movie-view';
 
@@ -7,13 +8,20 @@ class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', Genre: 'Sci-Fi', Director: 'Christopher Nolan', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', Genre: 'Drama', Director: 'Frank Darabont', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', Genre: 'Drama', Director: 'Rideley Scott', ImagePath: '...'}
-      ],
+      movies: [],
       selectedMovie: null
-    };
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://myflixandchill.herokuapp.com/movies')
+    .then(response => {
+      this.setState({movies: response.data
+      });
+    })
+    .catch(erorr => {
+      console.error(erorr);
+    });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -31,7 +39,7 @@ class MainView extends React.Component {
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
           ))
         }
       </div>
