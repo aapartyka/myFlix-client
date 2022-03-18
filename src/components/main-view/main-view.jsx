63dacc5b8558d07e5bfeab4909b067b171a54844
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-18 21:18:40 +0100

    update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,41 +1,70 @@
 import React from 'react';
-  import { MovieCard} from '../movie-card/movie-card';
-  import {MovieView} from '../movie-view/movie-view';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
+import { MovieCard} from '../movie-card/movie-card';
+import {MovieView} from '../movie-view/movie-view';
 
 class MainView extends React.Component {
 
   constructor(){
     super();
+    // Initial state is set to null.
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', Genre: 'Sci-Fi', Director: 'Christopher Nolan', ImagePath: '...'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', Genre: 'Drama', Director: 'Frank Darabont', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', Genre: 'Drama', Director: 'Rideley Scott', ImagePath: '...'}
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null
     };
   }
 
+  componentDidMount(){
+    //Get movies/database from api.
+    axios.get('http://localhost:8080/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => { 
+      console.log(error);
+    });
+  }
+
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` property to that movie.*/
   setSelectedMovie(newSelectedMovie) {
     this.setState({selectedMovie: newSelectedMovie
     });
   }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user.*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
   
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
 
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
         {selectedMovie
           ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
           : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-          ))
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
         }
       </div>
     );
   }
+
 }
 export default MainView;
