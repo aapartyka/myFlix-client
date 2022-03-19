d758f0f0b765242cba02d1098ce8dcd08f5f1fb7
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-19 15:19:43 +0100

    update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import axios from 'axios';
 
 import { LoginView } from '../login-view/login-view';
-import { MovieCard} from '../movie-card/movie-card';
-import {MovieView} from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 class MainView extends React.Component {
 
