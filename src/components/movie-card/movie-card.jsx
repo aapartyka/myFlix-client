258a28a44289708d9edfdcfa56baf002f3cc613b
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-20 19:22:37 +0100

    update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import "./movie-card.scss";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render(){
     const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+
+    return(
+      <Card id="movie-card">
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title id="card-title">{movie.Title}</Card.Title>
+          <Card.Text id="card-text">{movie.Description}</Card.Text>
+          <Button id="card-button" onClick={() => onMovieClick(movie)} variant="link">Show more</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
