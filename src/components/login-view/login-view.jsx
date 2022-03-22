104c722fed6db054118d294ed76f05cc9c144b15
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-22 23:12:23 +0100

    update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,46 +1,102 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+import PropTypes from 'prop-types';
+import './login-view.scss';
+import { Link } from 'react-router-dom';
+import { Form, Button, Container, Row, Col, Card, CardGroup } from 'react-bootstrap';
 
 export function LoginView(props) {
+    const [ username, setUsername ] = useState('');
+    const [ password, setPassword ] = useState('');
 
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+    // Declare hook for each input.
+    const [ usernameErr, setUsernameErr ] = useState('');
+    const [ passwordErr, setPasswordErr ] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    /* Send a request to the server for authentication */
-      axios.post('https://myflixandchill.herokuapp.com/login', {
-        Username: username,
-        Password: password
-      })
-      .then(response => {
-        const data = response.data;
-        props.onLoggedIn(data);
-      })
-      .catch(e => {
-        console.log('no such user')
-      });
-  };
+    // validate user inputs
+    const validate = () => {
+        let isReq = true;
 
-  const handleSignup = (e) => {
-    console.log('Route to Reistration view.');
-  }
+        if(!username){
+            setUsernameErr('Username required');
+            isReq = false;
+        }else if(username.length < 4){
+            setUsernameErr('Username must be at least 4 characters long.');
+            isReq = false;
+        }
+        if(!password){
+            setPasswordErr('Password required');
+            isReq = false;
+        }else if(password.length < 5){
+            setPassword('Password must be at least 5 characters long.');
+            isReq = false;
+        }
 
-  return (
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
+        return isReq;
+    }
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-      <Button variant="secondary" type="link" onClick={handleSignup}>Sign Up</Button>
-    </Form>
-  )
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const isReq = validate();
+        if(isReq) {
+            /* Send request to the server for authentication. */
+            axios.post(`https://myflixandchill.herokuapp.com/login`, {
+                Username: username,
+                Password: password
+            })
+                .then(response =>{
+                    const data = response.data;
+                    props.onLoggedIn(data);
+                })
+                .catch(error => {
+                    console.log(error, 'no such user');
+                });
+        }
+    };
+
+    return (
+        <Container className="profile-view" align="center">
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Please login</Card.Title>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="formUsername">
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control type="text" onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
+                                        {/* Code added here to display validation error. */}
+                                        {usernameErr && <p>{usernameErr}</p>}
+                                    </Form.Group>
+
+                                    <Form.Group className="mb-3" controlId="formPassword">
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control type="password" onChange={e => setPassword(e.target.value)} placeholder="Enter password" />
+                                        {/* Code added here to display validation error. */}
+                                        {passwordErr && <p>{passwordErr}</p>}
+                                    </Form.Group>
+                                    <div className="mt-3">
+                                        <Button variant="success" type="submit" onClick={handleSubmit}>Submit</Button>
+                                        <Link to="/register">
+                                            <Button className="ml-3" variant="secondary">Register now</Button>
+                                        </Link>
+                                    </div>
+
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
+
+LoginView.propTypes = {
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func.isRequired,
+};
