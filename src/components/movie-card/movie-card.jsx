ddcd81838bcc1b298f0b811dc9fe5b6517d6b104
aapartyka <76936962+aapartyka@users.noreply.github.com>
2022-03-29 14:24:39 +0200

    refactor: removed unnecessary  property

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -39,6 +39,5 @@ MovieCard.propTypes = {
       Title: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired,
       ImagePath: PropTypes.string.isRequired,
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    }).isRequired
 };
